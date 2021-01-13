dddbc82298a3becbbcbaf886e20558eb55e461ac
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2021-01-13 20:50:07 -0300

    criando subslides

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { useValue, onScrollEvent, interpolateColor } from 'react-native-redash';
-import Animated from 'react-native-reanimated';
+import Animated, { multiply } from 'react-native-reanimated';
 
 import Slide, { SLIDE_HEIGHT } from './Slide';
+import Subslide from './Subslide';
 
-const { width, height } = Dimensions.get('window');
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -14,26 +16,65 @@ const styles = StyleSheet.create({
 
   slider: {
     height: SLIDE_HEIGHT,
-    borderBottomRightRadius: 75,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
 
   footer: {
     flex: 1,
   },
+
+  footerContent: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
 });
 
+const slides = [
+  {
+    title: 'Relaxed',
+    subtitle: 'Find Your Outfits',
+    description:
+      'Confused about your outfit? Dont worry! Find the best outfit here!',
+    color: '#BFEAF5',
+  },
+  {
+    title: 'Playful',
+    subtitle: 'Hear it First, Wear it First',
+    description:
+      'Hating the clothes in your wardrobe? Explore hundreds of outfit ideas',
+    color: '#BEECC4',
+  },
+  {
+    title: 'Excentric',
+    subtitle: 'Your Style, Your Way',
+    description:
+      'Create your individual & unique style and look amazing everyday',
+    color: '#FFE4D9',
+  },
+  {
+    title: 'Funky',
+    subtitle: 'Look Good, Feel Good',
+    description:
+      'Discover the latest trends in fashion and explore your personality',
+    color: '#FFDDDD',
+  },
+];
+
 const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
   const x = useValue(0);
   const onScroll = onScrollEvent({ x });
   const backgroundColor = interpolateColor(x, {
-    inputRange: [0, width, width * 2, width * 3],
-    outputRange: ['#BFEAF5', '#BEECC4', '#FFE4D9', '#FFDDDD'],
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
   });
 
   return (
     <View style={styles.container}>
       <Animated.View style={[styles.slider, { backgroundColor }]}>
         <Animated.ScrollView
+          ref={scroll}
           horizontal
           snapToInterval={width}
           decelerationRate="fast"
@@ -42,19 +83,40 @@ const Onboarding = () => {
           scrollEventThrottle={1}
           {...{ onScroll }}
         >
-          <Slide label="Relaxed" />
-          <Slide label="Playful" right />
-          <Slide label="Excentric" />
-          <Slide label="Funky" right />
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
+          ))}
         </Animated.ScrollView>
       </Animated.View>
       <View style={styles.footer}>
         <Animated.View
           style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
         />
-        <View
-          style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 75 }}
-        />
+        <Animated.View
+          style={[
+            styles.footerContent,
+            {
+              width: width * slides.length,
+              flex: 1,
+              transform: [{ translateX: multiply(x, -1) }],
+            },
+          ]}
+        >
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              onPress={() => {
+                if (scroll.current) {
+                  scroll.current
+                    .getNode()
+                    .scrollTo({ x: width * (index + 1), animated: true });
+                }
+              }}
+              last={index === slides.length - 1}
+              {...{ subtitle, description }}
+            />
+          ))}
+        </Animated.View>
       </View>
     </View>
   );
