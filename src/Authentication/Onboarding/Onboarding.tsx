bb30c4713437797d4cfb879522acbf851c199436
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2021-01-13 18:29:05 -0300

    criando os slides

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,8 +1,62 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { useValue, onScrollEvent, interpolateColor } from 'react-native-redash';
+import Animated from 'react-native-reanimated';
 
-const ComponentName = () => (
-  <View style={{ flex: 1, backgroundColor: 'cyan' }} />
-);
+import Slide, { SLIDE_HEIGHT } from './Slide';
 
-export default ComponentName;
+const { width, height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  slider: {
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: 75,
+  },
+
+  footer: {
+    flex: 1,
+  },
+});
+
+const Onboarding = () => {
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: [0, width, width * 2, width * 3],
+    outputRange: ['#BFEAF5', '#BEECC4', '#FFE4D9', '#FFDDDD'],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          <Slide label="Relaxed" />
+          <Slide label="Playful" right />
+          <Slide label="Excentric" />
+          <Slide label="Funky" right />
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <View
+          style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 75 }}
+        />
+      </View>
+    </View>
+  );
+};
+export default Onboarding;
